7c052e4cc0f6facbd5c1e3a92cf1a3609ee6a769
Dusik <domsztof@gmail.com>
2025-09-24 13:24:04 +0200

    fix: main logic substing + async

--- šimek/main.py
+++ šimek/main.py
@@ -4,7 +4,6 @@ import random
 import datetime as dt
 import disnake
 from disnake import Message
-from disnake.ext import commands
 import requests
 from collections import defaultdict, Counter
 
@@ -18,8 +17,7 @@ import pickle
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 TEXT_SYNTH_TOKEN = os.getenv('TEXT_SYNTH_TOKEN')
-PREFIX = os.getenv('BOT_PREFIX')
-REPLIES = ("Ano.", "Ne.", "Perhaps.")
+REPLIES = ("Ano.", "Ne.","Ano.", "Ne.", "Perhaps.", "Ano.", "Ne.", "Perhaps." ,"Možná.", "Pravděpodobně.", "bruh", "nemám tušení") #repeat ano/ne/perhaps to give it more common occurence
 
 MOT_HLASKY = schdic.MOT_HLASKY
 LINUX_COPYPASTA = schdic.LINUX_COPYPASTA
@@ -29,7 +27,7 @@ MARKOV_FILE = "markov_twogram.pkl"
 
 # add intents for bot and command prefix for classic command support
 intents = disnake.Intents.all()
-client = disnake.ext.commands.Bot(command_prefix=PREFIX, intents=intents)
+client = disnake.Client(intents=intents)
 
 # Trigram Markov chain functions
 
@@ -85,6 +83,13 @@ def markov_chain(messages, max_words=30):
 
 # trigram Markov chain functions end
 
+# we use a evil class magic to hack match case to check for substrings istead of exact matches
+# https://stackoverflow.com/a/78046484
+class Substring(str):
+    def __eq__(self, other):
+        return other.__contains__(self)
+
+# evil hack end
 
 async def do_response(reply: str, m: Message, chance=10, reaction=False):
     '''
@@ -113,7 +118,7 @@ async def on_message(m: Message):
         return
     if not hasattr(client, "last_reaction_time"):
         client.last_reaction_time = {}    
-    now = dt.datetime.utcnow()
+    now = dt.datetime.now()
     last_time = client.last_reaction_time.get(m.channel.id)
     if last_time and (now - last_time).total_seconds() < 60:
         return            
@@ -132,83 +137,82 @@ async def on_message(m: Message):
     if m.channel.id not in ALLOW_CHANNELS:
         return
     
-    match m.content.lower():
+    # we are matching whole substrings now, not exact matches, only one case will be executed, if none match, default case will be executed 
+    match Substring(m.content.lower()):
         case "hodný bot":
-            do_response("🙂", m, chance=1, reaction=True)
+            await do_response("🙂", m, chance=1, reaction=True)
         case "windows":
-            do_response("😔", m, chance=2, reaction=True)
+            await do_response("😔", m, chance=4, reaction=True)
         case "debian":
-            do_response("💜", m, chance=2, reaction=True)
+            await do_response("💜", m, chance=4, reaction=True)
         case "všechno nejlepší":
-            do_response("🥳", m, chance=1, reaction=True)
-        case "kdo je negr?":
-            do_response("Decim je negr.", m, chance=1)
-        case "kdo je based?":
-            do_response("To jsem já!", m, chance=1)
+            await do_response("🥳", m, chance=1, reaction=True)
         case "linux" | "gnu/linux":
-            do_response("🐧", m, chance=10, reaction=True)
-            do_response(LINUX_COPYPASTA, m, chance=10)
+            await do_response("🐧", m, chance=10, reaction=True)
+            await do_response(LINUX_COPYPASTA, m, chance=10)
         case "hilfe" | "pomoc" | "pomocí" | "help":
-            do_response(f'''„{MOT_HLASKY[random.randint(0, len(MOT_HLASKY) - 1)]}“
+            await do_response(f'''„{MOT_HLASKY[random.randint(0, len(MOT_HLASKY) - 1)]}“
                                                                                 - Mistr Oogway, {random.randint(470,480)} př. n. l.''', m, chance=3)        
         case "novinky":
-            do_response("😖", m, chance=3, reaction=True)
-            do_response("Přestaň postovat cringe, bro.", m, chance=10)
+            await do_response("😖", m, chance=3, reaction=True)
+            await do_response("Přestaň postovat cringe, bro.", m, chance=10)
         case "drž hubu":
-            do_response("Ne, ty. 😃", m, chance=1)
+            await do_response("ok", m, chance=1)
+            client.last_reaction_time[m.channel.id] = dt.datetime.now() + dt.timedelta(minutes=5) # 5 minute timeout after being told to shut up
+            return # skip setting the time again at the end of the function    
         case "jsem":
-            do_response(f'Ahoj, {" ".join(m.content.split("jsem")[1].split(".")[0].split(",")[0].split(" ")[1:])}. Já jsem táta.', m, chance=4)    
+            await do_response(f'Ahoj, {" ".join(m.content.split("jsem")[1].split(".")[0].split(",")[0].split(" ")[1:])}. Já jsem táta.', m, chance=4)    
         case "kdo":
-            do_response('kdo se ptal?', m, chance=3)
+            await do_response('kdo se ptal?', m, chance=3)
         case "anureysm" | "aneuerysm" | "brain damage" | "brian damage":
-            do_response('https://www.youtube.com/watch?v=kyg1uxOsAUY', m, chance=1)
+            await do_response('https://www.youtube.com/watch?v=kyg1uxOsAUY', m, chance=2)
         case "schizo":
-            do_response('never forgeti', m, chance=4)    
+            await do_response('never forgeti', m, chance=4)    
         case "?":
-            do_response(f'{random.choice(REPLIES)}', m, chance=6)        
+            await do_response(f'{random.choice(REPLIES)}', m, chance=6)        
         case "proč" | "proc":
-            do_response("skill issue", m, chance=8)
+            await do_response("skill issue", m, chance=8)
         case "jsi":
-            do_response(f'Tvoje máma je {" ".join(m.content.split("jsi")[1].split(" ")[1:])}.', m, chance=8)
+            await do_response(f'Tvoje máma je {" ".join(m.content.split("jsi")[1].split(" ")[1:])}.', m, chance=8)
         case "negr":
-            do_response(':sad:', m, chance=10)
-            do_response(':+1:', m, chance=30)
+            await do_response(':sad:', m, chance=10)
+            await do_response(':+1:', m, chance=30)
         case "israel" | "izrael":
-            do_response(':pensive:', m, chance=5)
+            await do_response(':pensive:', m, chance=5)
         case "mama" | "mamá" | "mami" | "mommy" | "mamka" | "mamko":
             apiCall = requests.get("https://www.yomama-jokes.com/api/v1/jokes/random/")
             if apiCall.status_code == 200:
-                do_response(f'{apiCall.json()["joke"]}', m, chance=4)
+                await do_response(f'{apiCall.json()["joke"]}', m, chance=4)
         case "lagtrain":
-            do_response("https://www.youtube.com/watch?v=UnIhRpIT7nc", m, chance=1)
+            await do_response("https://www.youtube.com/watch?v=UnIhRpIT7nc", m, chance=1)
         case "cum zone":
-            do_response("https://www.youtube.com/watch?v=j0lN0w5HVT8", m, chance=1)
+            await do_response("https://www.youtube.com/watch?v=j0lN0w5HVT8", m, chance=1)
         case "crab rave":
-            do_response("https://youtu.be/LDU_Txk06tM?t=75", m, chance=1)
+            await do_response("https://youtu.be/LDU_Txk06tM?t=75", m, chance=1)
         case "já jo":
-            do_response("já ne", m, chance=2)
+            await do_response("já ne", m, chance=2)
         case "já ne":
-            do_response("já jo", m, chance=2)
+            await do_response("já jo", m, chance=2)
         case "chci se zabít" | "suicidal":
-            do_response("omg don't kill yourself, ur too sexy, haha",m, chance=1)
+            await do_response("omg don't kill yourself, ur too sexy, haha",m, chance=1)
         case "v píči" | "v pici":
-            do_response("stejně tak moc v píči jako já včera večer v tvojí mámě loool",m,chance=10)
+            await do_response("stejně tak moc v píči jako já včera večer v tvojí mámě loool",m,chance=10)
         case "buisness" | "buisnes" | "buissnes" | "bussiness" | "bussines":
-            do_response("KÁMO lmao ukažte si na toho blbečka, co neumí napsat 'business' XDDDD :index_pointing_at_the_viewer: příště raději napiš 'byznys' dík :)",m,chance=1)´
+            await do_response("KÁMO lmao ukažte si na toho blbečka, co neumí napsat 'business' XDDDD :index_pointing_at_the_viewer: příště raději napiš 'byznys' dík :)",m,chance=1)
         case "reminder":
-            do_response("kind reminder: ur a bitch :)", m, chance=4)
+            await do_response("kind reminder: ur a bitch :)", m, chance=4)
         case "youtu.be" | "youtube.com":
-            do_response(RECENZE[random.randint(0,len(RECENZE)-1)],m,chance=1)
+            await do_response(RECENZE[random.randint(0,len(RECENZE)-1)],m,chance=1)
         case "špatný bot" | "spatny bot":
-            do_response("i'm trying my best :pensive:",m,chance=1)
+            await do_response("i'm trying my best :pensive:",m,chance=1)
         case "podle mě" | "myslím si" | "myslim si":
-            do_response(f'{random.choice(["souhlasím","nesouhlasím",""])}')      
+            await do_response(f'{random.choice(["souhlasím","nesouhlasím",""])}')      
         case _:
-            do_response("Mňau", m, chance=10000)
-            do_response("Pipík", m, chance=500000)
-            do_response("víš co? raději drž hubu, protože z tohohle jsem chytil rakovinu varlat", m, chance=500000)
-            do_response("dissnul bych tě, ale budu hodnej, takže uhhh to bude dobrý :+1:", m, chance=1000000)
-            do_response('https://www.youtube.com/watch?v=kyg1uxOsAUY', m, chance=1000000)
-                       
-   client.last_reaction_time[m.channel.id] = dt.datetime.utcnow() 
+            await do_response(f'{random.choice(
+                ["Mňau",
+                 "víš co? raději drž hubu, protože z tohohle jsem chytil rakovinu varlat",
+                 "dissnul bych tě, ale budu hodnej, takže uhhh to bude dobrý :+1:",
+                 "https://www.youtube.com/watch?v=kyg1uxOsAUY",
+                 ])}', m, chance=500000)                      
+    client.last_reaction_time[m.channel.id] = dt.datetime.now() 
 client.run(TOKEN)
