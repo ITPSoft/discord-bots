b438bc2016db9f89d431c5be1a54e0d4f7a162c1
Adam Beneš <benes@toohka.org>
2026-02-28 23:06:49 +0100

    remove redundant f prefix

--- src/grossmann/main.py
+++ src/grossmann/main.py
@@ -784,7 +784,7 @@ async def request_role(
     ]
 
     appeal_votes[(ctx.author.id, role.role_id)] = AccessVoting(allow=0, deny=0, voters=[])
-    await channel.send(f"@here")
+    await channel.send("@here")
     await channel.send(embed=embed, components=buttons)
 
 
